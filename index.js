fa7987e8990244a837b53abac38b44293e6c5b46
Victoria Miranda <vitorianevesmiranda@gmail.com>
2021-12-08 15:39:43 -0300

    done

--- index.js
+++ index.js
@@ -8,16 +8,36 @@ const data = require('./data');
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
-mongoose
-  .connect(MONGODB_URI)
+mongoose.connect(MONGODB_URI)
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe.create({
+      title: "Grandma's Chicken",
+      level: "Grandma",
+      ingridients:["chicken", "coriander", "Grandma's love"],
+      cuisine: "Grandma's Brazilian cuisine",
+      dishType: "meal",
+      image: "",
+      duration: 60,
+      creator: "Best grandma ever"
+    })
+
+    Recipe.insertMany(data)
+    console.log(`Log data inside db: ${data}`)
   })
+
+    Recipe.findOneAndUpdate( {title: "Chocolate Chip Cookies"}, {duration:60}, {new: true})
+      .then( () =>  {console.log ("update Carrot Cake")})
+
+      Recipe.deleteOne( { title: "Orange and Milk-Braised Pork Carnitas"})
+        .then( () => {console.log('Deleted Orange and Milk-Braised Pork Carnitas')})
+    
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+mongoose.connection.close()
